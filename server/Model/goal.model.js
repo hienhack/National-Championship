fa53e33b86721b87612ef7dd6b841e28daad7385
20120472 <hienthai.030102@gmail.com>
2023-06-17 10:59:15 +0700

    Add Goal model

---
+++ server/Model/goal.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const goalSchema = new Schema({
+    seasonId: Schema.Types.ObjectId,
+    time: Number, 
+    type: String, 
+    clubId: Schema.Types.ObjectId, 
+    scoredPlayer: Schema.Types.ObjectId,
+    assistedPlayer: Schema.Types.ObjectId
+});
+
+const goalModel = new mongoose.model('Goal', goalSchema, 'Goal');
+module.exports = goalModel;
