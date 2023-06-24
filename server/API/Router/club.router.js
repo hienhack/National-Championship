e33aa8aa0ca1f4fd185b6886fee38fd58b3ec7ea
20120472 <hienthai.030102@gmail.com>
2023-06-24 09:06:00 +0700

    add new end point

--- server/API/Router/club.router.js
+++ server/API/Router/club.router.js
@@ -8,6 +8,19 @@ router.post('/add-player', clubController.addPlayer);
 router.post('/update', uploader.single("image"), clubController.updateClub);
 router.post('/delete', clubController.deleteClub);
 router.post('/delete-player', clubController.deletePlayerFromClub);
+
+// trả về thông _id, name, image, coach, players list, mỗi player có shirtNumber, image, name
 router.get('/:clubId/:seasonId', clubController.getClub);
+
+
+
+// Tìm tất cả các mùa đội theo query
+// http://localhost:5000/club?name=adfds&seasonId
+// mỗi đội có thông tin _id, name, image
+// query chỉ có name -> trả về tất cả có tên khớp, sử dụng regex case insensitive để match
+ // query chỉ có seasonId ->  trả về tất cả trong mùa giải
+ // có cả name và seasonId -> có tên khớp và có trong mùa giải (này viểt ra thoi, chưa cần xử lý)
+ // không có gì, trả về tất cả
+router.get('/', (req, res) => res.status(501).send({message: "not implemented"}));
 
 module.exports = router;
