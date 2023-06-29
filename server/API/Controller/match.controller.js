b216711a2553f5d4119b9b245fc890341d46f967
20120467 <phuochainguyen2012@gmail.com>
2023-06-29 18:25:13 +0700

    fix addGoal and deleteGoal

--- server/API/Controller/match.controller.js
+++ server/API/Controller/match.controller.js
@@ -34,12 +34,12 @@ class MatchController {
         //    {playerId: "a43454545", name: "asdf adf a", shirtNumber: 10, type: sub}
         // ]
 
-        async function listPlayers(listPlayers){
+        async function listPlayers(listPlayers) {
             var list = [];
             for (let i = 0; i < listPlayers.length; i++) {
                 const p = listPlayers[i];
                 const player = await playerModel.findById(p.playerId);
-                const {name, ...data} = player;
+                const { name, ...data } = player;
 
                 var rs = new Object();
                 rs.playerId = p.playerId;
@@ -51,12 +51,12 @@ class MatchController {
             return list;
         }
         for (let index = 0; index < club1.seasons.length; index++) {
-            if (club1.seasons[index].seasonId.equals(match.seasonId)){
+            if (club1.seasons[index].seasonId.equals(match.seasonId)) {
                 match.club1.players = await listPlayers(club1.seasons[index].players);
             }
         }
         for (let index = 0; index < club2.seasons.length; index++) {
-            if (club2.seasons[index].seasonId.equals(match.seasonId)){
+            if (club2.seasons[index].seasonId.equals(match.seasonId)) {
                 match.club2.players = await listPlayers(club2.seasons[index].players);
             }
         }
@@ -66,7 +66,7 @@ class MatchController {
         // match.goals = [ 
         //      {club: 1, time: 30, type: "p", scoredPlayer: "adfads", assistedPlayer: "adfads" }
         // ]
-        if (match.goals != undefined){
+        if (match.goals != undefined) {
             match.goals = await Promise.all(match.goals.map(async id => {
                 const goal = await goalModel.findById(id);
                 var rs = new Object();
@@ -76,7 +76,7 @@ class MatchController {
                 const clubGoal = await clubModel.findById(goal.clubId);
                 const scoredPlayer = await playerModel.findById(goal.scoredPlayerId);
                 const assistedPlayer = await playerModel.findById(goal.assistedPlayerId);
-    
+
                 if (clubGoal._id.equals(club1._id)) {
                     rs.club = 1;
                 } else if (clubGoal._id.equals(club2._id)) {
@@ -86,16 +86,15 @@ class MatchController {
                 if (assistedPlayer) {
                     rs.assistedPlayer = assistedPlayer.name;
                 }
-    
+
                 return rs;
             }));
-        }else{
+        } else {
             match.goals = [];
         }
         if (match.cards != undefined) {
             match.cards = await Promise.all(match.cards.map(async card => {
                 const player = await playerModel.findById(card.playerId);
-                console.log(player);
                 var rs = {};
                 rs.club = card.club;
                 rs.playerId = card.playerId;
@@ -103,7 +102,7 @@ class MatchController {
                 rs.playerName = player.name;
                 return rs;
             }));
-        }else {
+        } else {
             match.cards = [];
         }
         delete match.club1Id;
@@ -119,7 +118,6 @@ class MatchController {
     async getAll(req, res) {
         const { round, seasonId, result } = req.query;
 
-        console.log(result);
         // Tìm tất cả các trận đấu có round và seasonId hoặc clubId
         // Vì là query nên có thể có field = null
         var queries = {};
@@ -129,20 +127,20 @@ class MatchController {
         }
         if (seasonId) {
             queries.seasonId = seasonId;
-        }else {
-            res.status(400).send({message: "Request invalid"});
+        } else {
+            res.status(400).send({ message: "Request invalid" });
             return;
         }
         // if (clubId) {
         //     queries.$or = [{ "club1Id": clubId }, { "club2Id": clubId }];
         // }
-        if ( result ) {
+        if (result) {
             var matches = await matchModel.find(queries).select("_id club1Id club2Id datetime isPlayed round stadium result").lean();
-        }else {
+        } else {
             var matches = await matchModel.find(queries).select("_id club1Id club2Id datetime isPlayed round stadium").lean();
         }
-        
-        for ( const match of matches ) {
+
+        for (const match of matches) {
             const club1 = await clubModel.findById(match.club1Id);
             const club2 = await clubModel.findById(match.club2Id);
             match.club1 = {
@@ -157,16 +155,15 @@ class MatchController {
             };
         }
 
-        const season = await seasonModel.findOne({_id: seasonId});
+        const season = await seasonModel.findOne({ _id: seasonId });
         const numberOfClub = season.clubs.length;
-        const numberOfRound = numberOfClub * 2 - 2 ;
-        res.status(200).send({ message: "success", data: {matches, numberOfRound} });
+        const numberOfRound = numberOfClub * 2 - 2;
+        res.status(200).send({ message: "success", data: { matches, numberOfRound } });
         return;
 
     }
 
     async create(req, res) {
-        console.log(req);
         const match = req.body;
         match.isPlayed = false;
         match.goals = [];
@@ -216,8 +213,7 @@ class MatchController {
 
     async addGoal(req, res) {
         const { matchId, goal } = req.body;
-
-        const match = await matchModel.findById(matchId).exec();
+        const match = await matchModel.findById(matchId);
         if (!match) {
             res.status(400).send({ message: "Match not found" });
             return;
@@ -225,22 +221,21 @@ class MatchController {
 
         try {
             const goalDoc = new goalModel(goal);
-            goalDoc.save();
-
-  
 
             if (goalDoc.clubId.equals(match.club1Id)) {
-                match.result.club1++;
-            } else if (goal.clubId.equals(match.club2Id)) {
-                match.result.club2++;
-            } else {
-                res.status(400).send({ message: "Invalid scored club" });
-                return;
+                console.log("club1");
+                await matchModel.findByIdAndUpdate(matchId, { $inc: { "result.club1": 1 } });
+            }
+            if (goalDoc.clubId.equals(match.club2Id)) {
+                console.log("club2");
+                await matchModel.findByIdAndUpdate(matchId, { $inc: { "result.club2": 1 } });
             }
 
+            console.log(1);
             match.goals.push(goalDoc._id);
-
+            goalDoc.save();
             match.save();
+            console.log(match);
             res.status(200).send({ message: "Added Goal successfully" });
             return;
 
@@ -250,19 +245,29 @@ class MatchController {
         }
     }
 
-    async deleteGoal(req,res){
+    async deleteGoal(req, res) {
         const goalId = req.body.goalId;
         const matchId = req.body.matchId;
-
+        const goal = await goalModel.findById(goalId);
         const match = await matchModel.findById(matchId);
         try {
             match.goals.pull(goalId);
             match.save();
+
+            if (goal.clubId.equals(match.club1Id)) {
+                console.log("club1");
+                await matchModel.findByIdAndUpdate(matchId, { $inc: { "result.club1": -1 } });
+            }
+            if (goal.clubId.equals(match.club2Id)) {
+                console.log("club2");
+                await matchModel.findByIdAndUpdate(matchId, { $inc: { "result.club2": -1 } });
+            }
+
             await goalModel.findByIdAndDelete(goalId);
-            res.status(200).send({message: 'Deleted goal successfully!'});
+            res.status(200).send({ message: 'Deleted goal successfully!' });
             return;
         } catch (error) {
-            res.status(400).send({message: "Deleted goal failed"});
+            res.status(400).send({ message: "Deleted goal failed" });
             return;
         }
     }
@@ -287,18 +292,18 @@ class MatchController {
         }
     }
 
-    async deleteCard(req,res){
+    async deleteCard(req, res) {
         const cardId = req.body.cardId;
         const matchId = req.body.matchId;
 
         const match = await matchModel.findById(matchId);
         try {
-            match.cards.pull({_id: cardId});
+            match.cards.pull({ _id: cardId });
             match.save();
-            res.status(200).send({message: 'Deleted card successfully!'});
+            res.status(200).send({ message: 'Deleted card successfully!' });
             return;
         } catch (error) {
-            res.status(400).send({message: "Deleted card failed"});
+            res.status(400).send({ message: "Deleted card failed" });
             return;
         }
     }
