5a759d26240315a5f2859f063c37fb5fb22582b3
20120472 <hienthai.030102@gmail.com>
2023-07-01 12:03:09 +0700

    remove unused code

--- server/API/Router/index.js
+++ server/API/Router/index.js
@@ -1,7 +1,6 @@
 const seasonRouter = require('./season.router');
 const clubRouter = require('./club.router');
 const playerRouter = require('./player.router');
-const scheduleRouter = require('./schedule.router');
 const matchRouter = require('./match.router');
 const reportRouter = require('./report.router');
 
@@ -10,6 +9,5 @@ module.exports = function(app) {
     app.use('/api/club', clubRouter);
     app.use('/api/player', playerRouter);
     app.use('/api/match', matchRouter);
-    app.use('/api/schedule', scheduleRouter);
     app.use('/api/report', reportRouter);
 }
